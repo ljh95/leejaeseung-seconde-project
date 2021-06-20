6ccbc6a4a5598ae557c5abdc9dd03196c92c0748
jongho Lee <ilegwmam@gmail.com>
2021-06-20 18:54:19 +0900

    ♻️ AuthLayout 따로빼기

--- src/auth/container/Login.js
+++ src/auth/container/Login.js
@@ -1,62 +1,36 @@
-import { Row, Col, Typography, Form, Input, Button } from 'antd';
+import { Form, Input, Button } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import AuthLayout from '../component/AuthLayout';
 
 export default function Login() {
   return (
     <>
-      <Row justify="center" style={{ marginTop: 100 }}>
-        <Col>
-          <Typography.Title style={{ fontFamily: 'Caligrappy' }}>
-            찾아야 한다.
-          </Typography.Title>
-        </Col>
-      </Row>
-      <Row justify="center">
-        <Col>
-          <Form
-            initialValues={{ remember: true }}
-            style={{ width: 300, marginTop: 50 }}
-            onFinish={() => {}}
-          >
-            <Form.Item
-              name="username"
-              rules={[
-                { required: true, message: 'Please input your Username!' },
-              ]}
-            >
-              <Input
-                autoFocus
-                prefix={<UserOutlined />}
-                placeholder="Username"
-              />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                { required: true, message: 'Please input your Password!' },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined />}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                style={{ width: '100%' }}
-              >
-                Log in
-              </Button>
-              Or <Link to="/signup">register now!</Link>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
+      <AuthLayout onFinish={() => {}}>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: 'Please input your Username!' }]}
+        >
+          <Input autoFocus prefix={<UserOutlined />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Please input your Password!' }]}
+        >
+          <Input
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+            Log in
+          </Button>
+          Or <Link to="/signup">register now!</Link>
+        </Form.Item>
+      </AuthLayout>
     </>
   );
 }
